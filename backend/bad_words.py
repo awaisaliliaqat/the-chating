e666b803fa61fb53512938192ef40e032442e64c
Anas Wahab <anas.wahab@tmcltd.ai>
2026-06-01 22:20:58 +0500

    Expand bad words: +100 words covering leet speak, slurs, threats, scams, drugs, harassment

--- backend/bad_words.py
+++ backend/bad_words.py
@@ -1,35 +1,62 @@
 # Bad words list — add or remove words here at any time
 BAD_WORDS = [
-    # Profanity
-    "fuck","fucking","fucker","fucked","fuk","f**k","f***","fck","fuk",
-    "shit","shite","bullshit","bs","sh*t","sht",
-    "bitch","b*tch","btch","biatch",
-    "ass","asshole","arsehole","arse","a**","azzhole",
-    "bastard","damn","crap","piss","cunt","c**t",
-    "dick","d*ck","cock","c*ck","penis","d1ck",
-    "pussy","p*ssy","vagina","puss",
-    "whore","wh*re","slut","sl*t","hoe","thot",
-    "wtf","stfu","gtfo","ffs",
+    # ── Profanity & variations ────────────────────────────────────────────────
+    "fuck","fucking","fucker","fucked","fucks","fuk","f**k","f***","fck","fukk",
+    "f u c k","wtf","wth",
+    "shit","shite","bullshit","sh*t","sht","shitt","$hit",
+    "bitch","b*tch","btch","biatch","b1tch","bytch",
+    "ass","asshole","arsehole","arse","a**","azzhole","a$$",
+    "bastard","damn","crap","piss","pissed","cunt","c**t","c*nt",
+    "dick","d*ck","cock","c*ck","d1ck","d!ck","dikk",
+    "pussy","p*ssy","vagina","puss","p***y",
+    "whore","wh*re","slut","sl*t","hoe","thot","skank","tramp",
+    "boobs","tits","t*ts","b00bs","naked","nudes","nude",
+    "sex","sexy","sexting","porn","p0rn","xxx","nsfw",
+    "stfu","gtfo","ffs","omfg","afk","smh",
 
-    # Slurs (racial, religious, orientation)
-    "nigger","nigga","n*gger","n*gga","niga",
-    "faggot","fag","f*ggot","dyke","homo",
-    "spic","chink","gook","kike","wetback","cracker","honky",
-    "retard","retarded","r*tard","tard",
+    # ── Evasion / leet speak ──────────────────────────────────────────────────
+    "fvck","fvk","fck","ph*ck","phuck",
+    "sh!t","$hit","5hit",
+    "b!tch","b1tch",
+    "a$$hole","@sshole",
+    "c0ck","d!ck","d1ck",
 
-    # Threats / violent
-    "kill yourself","kys","kill urself","kys yourself",
-    "i will kill","i'll kill","gonna kill","want to kill","ill kill",
-    "die bitch","go die","drop dead","kill u","kill you",
-    "rape","r*pe","raped","raping","rapist",
-    "bomb","terrorist","shoot you","stab you","i will hurt",
+    # ── Racial / ethnic slurs ─────────────────────────────────────────────────
+    "nigger","nigga","n*gger","n*gga","niga","n1gga","nigg","negro",
+    "faggot","fag","f*ggot","dyke","homo","queer","tranny",
+    "spic","chink","gook","kike","wetback","cracker","honky","coon",
+    "beaner","towelhead","sandnigger","zipperhead","jap","paki",
 
-    # Harassment
-    "idiot","moron","stupid","loser","freak",
-    "ugly","fat pig","go to hell","shut up","dumb",
-    "scammer","spam","spammer",
+    # ── Ableist slurs ─────────────────────────────────────────────────────────
+    "retard","retarded","r*tard","tard","spastic","mong","cripple",
 
-    # Custom test words (remove when done testing)
+    # ── Threats & violence ────────────────────────────────────────────────────
+    "kill yourself","kys","kill urself","kys yourself","end yourself",
+    "i will kill","i'll kill","gonna kill","want to kill","ill kill","going to kill",
+    "die bitch","go die","drop dead","kill u","kill you","i'll hurt you",
+    "i want to kill","shoot you","stab you","gonna stab","im gonna kill",
+    "rape","r*pe","raped","raping","rapist","molest","molestation",
+    "bomb","bombing","terrorist","terrorism","jihad","suicide bomb",
+    "i will hurt","hurt you","beat you up","beat u up",
+    "cut yourself","self harm","harm yourself",
+
+    # ── Scams & spam ─────────────────────────────────────────────────────────
+    "scammer","scam","spammer","spam","phishing","hack you","hacking",
+    "send money","wire transfer","bitcoin scam","free money","click here",
+    "your account suspended","verify your account","you won",
+
+    # ── Drug references ───────────────────────────────────────────────────────
+    "cocaine","heroin","meth","methamphetamine","crack cocaine",
+    "buy drugs","sell drugs","weed dealer","drug dealer",
+
+    # ── Harassment ────────────────────────────────────────────────────────────
+    "idiot","moron","stupid","loser","freak","pathetic",
+    "ugly","fat pig","fat bitch","go to hell","shut up","dumb",
+    "piece of shit","son of a bitch","piece of crap","go fuck yourself",
+    "nobody likes you","you should die","worthless","useless piece",
+    "kill urself","end it all",
+
+    # ── Custom test ───────────────────────────────────────────────────────────
     "676767",
 ]
 
